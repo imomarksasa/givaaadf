1d8a86ec984a5de2bbc12f4f2249360b9dfa9048
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-03-03 20:00:56 +0300

    Update probot.js

--- probot.js
+++ probot.js
@@ -90,7 +90,8 @@ client.on('message',async message => {
     });
   }
   if(!args[2]) {
-    if(mention.bot) return message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**`);
+    if(mention.bot) return message.channel.send('**:heavy_multiplication_x:| ${message.content.split(' ')[1]} لم يتم العثور على**);
+    message.channel.send(`**${mention.username}, your :credit_card: balance is **`${credits[mention.id].credits}``);
   } 
   
   }
