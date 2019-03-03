f304ac0671604258c8cda98f4b6fd570b52dcc24
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-03-03 18:52:50 +0300

    Update probot.js

--- probot.js
+++ probot.js
@@ -48,15 +48,7 @@ message.channel.send(`**:moneybag: | ${message.author.username}, has transferrer
    if(err) console.log(err)
  });
 
-      let mariam = message.author.username
-message.channel.send('`' + mentionned.username + '`' + '** تم التحويل **'+ '`' + mariam + '`' + '**المبلغ**'+ (args[1]) + '** :dollar: **')
-
-}
-
-module.exports.help = {
-  name: "تحويل"
-}
-
+     
 
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 client.on("message", message => {
